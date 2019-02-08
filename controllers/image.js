d4bcc0f381877517b0a3487a7cd5433f65f729a9
pmurph0305 <pmurph0305@gmail.com>
2019-02-08 15:40:40 -0500

    Initial commit

---
+++ controllers/image.js
@@ -0,0 +1,35 @@
+const Clarifai = require('clarifai');
+
+//Clarifai api key.
+const app = new Clarifai.App({
+	apiKey: '68b7aff4f7f249a4a717359ae81506fa'
+});
+
+const handleApiCall = () => (req, res) => {
+    app.models.predict(Clarifai.FACE_DETECT_MODEL, req.body.input)
+    .then(data=> {
+        res.json(data);
+    })
+    .catch(err => res.status(400).json('Unable to work API'))
+};
+
+// Handles image put route.
+const handleImagePut = (db) => (req, res) => {
+    const { id, faces } = req.body;
+    //console.log(faces);
+    // select user and update entries.
+    db('users').where('id', '=', id)
+        .increment({
+            entries: faces
+        })
+        .returning('entries')
+    .then(entries => {
+        res.json(entries[0]);
+    })
+    .catch(err => res.status(400).json('Unable to get entries.'));
+};
+
+module.exports = {
+    handleImagePut: handleImagePut,
+    handleApiCall: handleApiCall
+};
---
+++ server.js
@@ -0,0 +1,48 @@
+const bcrypt = require('bcrypt-nodejs');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const express = require('express');
+const knex = require('knex')
+
+const register = require('./controllers/register');
+const signin = require('./controllers/signin');
+const image = require('./controllers/image');
+const profile = require('./controllers/profile');
+
+const db = knex({
+    client: 'pg',
+    connection: {
+      host : '127.0.0.1',
+      user : 'postgres',
+      password : 'test',
+      database : 'smartbrain'
+    }
+});
+
+const app = express();
+
+// parse body for json.
+app.use(bodyParser.json());
+app.use(cors());
+
+app.listen(8080, () => {
+    console.log("Listening..");
+});
+
+// This should not be here anymore, but leaving incase of future
+// updates to course that changes it.
+// app.get('/', (req, res) => {
+//     res.send(database.users);
+// });
+
+// can move arrow functions to inside the controller
+app.put('/image', image.handleImagePut(db))
+
+app.post('/imageurl', image.handleApiCall())
+
+//or keep them outside
+app.get('/profile/:id', (req, res) => { profile.handleProfileGet(req, res, db) });
+
+app.post('/register', (req, res) => { register.handleRegister(req, res, db, bcrypt) })
+
+app.post('/signin', (req, res) => { signin.handleSignin(req, res, db, bcrypt) })
