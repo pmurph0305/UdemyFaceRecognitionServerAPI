4ed7ebb62acd063261af70dec6ebaa2a98099da5
pmurph0305 <pmurph0305@gmail.com>
2019-02-08 16:23:53 -0500

    Adding database ssl and host

--- server.js
+++ server.js
@@ -12,10 +12,8 @@ const profile = require('./controllers/profile');
 const db = knex({
     client: 'pg',
     connection: {
-      host : 'postgresql-acute-75356',
-      user : 'postgres',
-      password : 'test',
-      database : 'smartbrain'
+      host : process.env.DATABASE_URL,
+      ssl: true,
     }
 });
 
