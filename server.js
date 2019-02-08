e14f91abe6af0e45b62bb582643bcbbca5465cea
pmurph0305 <pmurph0305@gmail.com>
2019-02-08 16:21:38 -0500

    Updating database

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ const profile = require('./controllers/profile');
 const db = knex({
     client: 'pg',
     connection: {
-      host : '127.0.0.1',
+      host : 'postgresql-acute-75356',
       user : 'postgres',
       password : 'test',
       database : 'smartbrain'
